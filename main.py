dc57863b609c8536f0b2de521a162fe1c036c68b
Fernando <fernandosanchez955@gmail.com>
2025-08-19 00:09:11 -0400

    Adding preliminary code to access the local MySQL server

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pandas as pd
-print("hello world")
+import mysql.connector
+import getpass
+
+user = input("Please provide user to the database: ")
+password = getpass.getpass("Please provide the password to connect to the database: ")
+conn = mysql.connector.connect(
+    host="localhost",   # or "localhost", depending on your setup
+    port=3306,          # default MySQL port
+    user="{}".format(user),
+    password="{}".format(password)
+)
+
+connection_status = conn.is_connected()
+print(connection_status)
