98b4cf670d0733ca407ea0e111f47fa80b851d85
Fernando <fernandosanchez955@gmail.com>
2025-08-19 22:34:30 -0400

    Moved the logic for connecting to a MYSQL database into its own function

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import pandas as pd
 import mysql.connector
 import getpass
 
-user = input("Please provide user to the database: ")
-password = getpass.getpass("Please provide the password to connect to the database: ")
-conn = mysql.connector.connect(
-    host="localhost",   # or "localhost", depending on your setup
-    port=3306,          # default MySQL port
-    user="{}".format(user),
-    password="{}".format(password)
-)
+def connectToDatabase():
+    user = input("Please provide user to the database: ")
+    password = getpass.getpass("Please provide the password to connect to the database: ")
+    conn = mysql.connector.connect(
+        host="localhost",   # or "localhost", depending on your setup
+        port=3306,          # default MySQL port
+        user="{}".format(user),
+        password="{}".format(password)
+    )
+    connection_status = conn.is_connected()
+    if connection_status:
+        print("Successfully connected to the database")
+        return conn
+    else:
+        print("Something went wrong - try again")
+        return -1
 
-connection_status = conn.is_connected()
-print(connection_status)
+#Ingest the sample csv file that we have produced with ChatGpt:
+path_for_expenses = input("Please provide the path for the sample csv: ")
+df =  pd.read_csv(path_for_expenses)
+print(df)
