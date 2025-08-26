a6f2f11b859d8418573619ea822541efe6892263
Fernando <fernandosanchez955@gmail.com>
2025-08-26 00:01:10 -0400

    Added some more logic to check the incoming column names of the batch

--- helperfunctions.py
+++ helperfunctions.py
@@ -13,3 +13,17 @@ def validate_schema(cursor_db, table_name, csv_df):
     output = cursor_db.fetchall()
     print(output)
     print(csv_column_names)
+    print(csv_df.dtypes)
+    #First validate that the names are the same 
+    extracted_names_db = set([tuplet[0] for tuplet in output])
+    csv_column_names_set = set(csv_column_names)
+    if extracted_names_db == csv_column_names_set:
+        print("The schema between the the csv file and the target database is consistent - insofar as column names are concerned")
+    else:
+        print("There are some inconsistencies in the columns between the two")
+        expected_in_mysql_table = extracted_names_db - csv_column_names_set
+        csv_not_in_table = csv_column_names_set - expected_in_mysql_table
+        print("In table not in csv: {}".format(expected_in_mysql_table))
+        print("In CSV not in table: {}".format(csv_not_in_table))
+
+    
