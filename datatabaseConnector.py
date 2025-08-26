de94dbe05caf8483d5dcd08bd39d491195deaa14
Fernando <fernandosanchez955@gmail.com>
2025-08-25 23:44:24 -0400

    Removed two functions to maintain class simple

--- datatabaseConnector.py
+++ datatabaseConnector.py
@@ -33,9 +33,3 @@ class DatabaseConnector:
 
     def commit_changes(self):
         self.conn.commit()
-    
-    def get_accessible_schemas(self, cursor):
-        cursor.execute("SHOW DATABASES")
-        container = cursor.fetchall()
-        applicable_databases = [database[0] for database in container if database[0] not in {'information_schema', 'performance_schema'}]
-        return applicable_databases
